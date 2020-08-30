079011c3177375c9f66728c243039878f0483dbe
rohit <kumbharrohit13@gmail.com>
2020-08-31 00:52:35 +0530

    Updated

---
+++ Inventory/src/main/java/com/computebook/inventory/InventoryApplication.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.computebook.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Rohit
+ *
+ */
+@SpringBootApplication
+public class InventoryApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryApplication.class, args);
+	}
+}
